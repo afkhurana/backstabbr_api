e00a5a8b5ac21fe21dc7b188c7fc8914f359216b
afkhurana <31321265+afkhurana@users.noreply.github.com>
2021-01-14 17:37:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='backstabbr_api',
-		version='1.0',
+		version='1.0.0',
 		description='Web-scraper API and Discord Bot for the online diplomacy program Backstabbr',
 		url='https://github.com/afkhurana/backstabbr_api',
 		author='Arjun Khurana',
 		author_email='afkhurana@gmail.com',
 		license='MIT',
-		packages=['backstabbr_api', 'backstabbr_bot'])
+		packages=['backstabbr_api', 'backstabbr_bot'],
+		install_requires=["discord.py, html5print, requests"])
